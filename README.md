e0a2510c62abc4acbf206f3b7e3da41544d4991f
Reyner Santonius <reynersantonius@Reyners-MBP.attlocal.net>
2022-02-25 17:40:17 -0800

    added mds files, updateon handle no javascript for projects section

---
+++ README.md
@@ -0,0 +1,5 @@
+Reyner Santonius
+
+A16537955
+
+https://cse134b-hw4-f4726.web.app/
--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init(){
+    changeDisplay();
+    renderSlide();
     updateSlide();
+}
+
+
+function changeDisplay(){
+    const projectsContainer = document.querySelector('.projects-container');
+    projectsContainer.classList.remove('projects-container');
+    projectsContainer.classList.add('projects-container-js');
+
+    const projectElements = document.querySelectorAll('.project-element-grid-container-2');
+    
+    for(let i=0; i<projectElements.length; i++){
+        projectElements[i].classList.remove('project-element-grid-container-2');
+        projectElements[i].classList.add('project-element-grid-container-2-js');
+    }
+
+    const aPrevSlide = document.querySelector('a.prev-slide');
+    aPrevSlide.classList.remove('prev-slide');
+    aPrevSlide.classList.add('prev-slide-js');
+    
+    const aNextSlide = document.querySelector('a.next-slide');
+    aNextSlide.classList.remove('next-slide');
+    aNextSlide.classList.add('next-slide-js');
+
+    const divSlideNumsHolder = document.querySelector('.slide-nums-holder');
+    divSlideNumsHolder.classList.remove('slide-nums-holder');
+    divSlideNumsHolder.classList.add('slide-nums-holder-js');
 }
 
 let slideIdx = 0;
 
 function renderSlide(){
-    const divProjects = document.querySelectorAll('.project-element-grid-container-2');
+    const divProjects = document.querySelectorAll('.project-element-grid-container-2-js');
     const spanSlideNums = document.querySelectorAll('.slide-num');
-    
-    console.log(`renderSlide(); idx = ${slideIdx}`);
+
     slideIdx %= divProjects.length;
     if (slideIdx < 0){
         slideIdx += divProjects.length
     }
-    console.log(`renderSlide(); idx = ${slideIdx}`);
 
     for(let i=0; i<divProjects.length; i++){
         divProjects[i].style.visibility = 'hidden';
@@ -34,17 +60,15 @@ function renderSlide(){
 }
 
 function updateSlide(){
-    const aPrevSlide = document.querySelector('a.prev-slide');
-    const aNextSlide = document.querySelector('a.next-slide');
+    const aPrevSlide = document.querySelector('a.prev-slide-js');
+    const aNextSlide = document.querySelector('a.next-slide-js');
 
     aPrevSlide.addEventListener('click', ()=>{
-        console.log('clicked');
         slideIdx -= 1;
         renderSlide();
     });
     
     aNextSlide.addEventListener('click', ()=>{
-        console.log('clicked');
         slideIdx += 1;
         renderSlide();
     });
